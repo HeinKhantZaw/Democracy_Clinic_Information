3c05de9b2395cd8e32edfd886f180a87a6b19503
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2021-02-04 22:17:27 +0630

    removed unused variables

--- src/views/MainPage/Sections/DoctorSection.js
+++ src/views/MainPage/Sections/DoctorSection.js
@@ -1,228 +1,225 @@
 import React from "react";
 import DocInfo from "./DocInfo";
-import {docData} from "../../../data/data";
+import { docData } from "../../../data/data";
 
-const styles = {
-  textCenter: {
-    textAlign: "center"
-  }
-};
-
-export default function DoctorSection(){
+export default function DoctorSection() {
   return (
-    <div>  
-      
+    <div>
       <details>
-  <summary>အထွေထွေရောဂါကု</summary>
-  {docData.GP.map((data, key)=>{
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
-  <details>
-  <summary>အထူးကုသမားတော်</summary>
-  {docData.Physician.map((data, key)=>{
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
-  <details>
-  <summary>သွားဆေးဘက်ဆိုင်ရာ</summary>
-  {docData.Dentist.map((data, key)=>{
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+        <summary>အထွေထွေရောဂါကု</summary>
+        {docData.GP.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
+      <details>
+        <summary>အထူးကုသမားတော်</summary>
+        {docData.Physician.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
+      <details>
+        <summary>သွားဆေးဘက်ဆိုင်ရာ</summary>
+        {docData.Dentist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>နား၊ နှာခေါင်း၊ လည်ချောင်းဆိုင်ရာ</summary>
-  {docData.Otorhinolaryngologist.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
- </details>
+      <details>
+        <summary>နား၊ နှာခေါင်း၊ လည်ချောင်းဆိုင်ရာ</summary>
+        {docData.Otorhinolaryngologist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-<details>
-  <summary>အာဟာရဆိုင်ရာ</summary>
-  {docData.Nutritionist.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>အာဟာရဆိုင်ရာ</summary>
+        {docData.Nutritionist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>သားဖွားမီးယပ်ပိုင်းဆိုင်ရာ</summary>
-  {docData.OG.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
- </details>
+      <details>
+        <summary>သားဖွားမီးယပ်ပိုင်းဆိုင်ရာ</summary>
+        {docData.OG.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
- <details>
-  <summary>အကြောအဆစ်ပိုင်းဆိုင်ရာ</summary>
-  {docData.Physiotherapist.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>အကြောအဆစ်ပိုင်းဆိုင်ရာ</summary>
+        {docData.Physiotherapist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>နှလုံးအထူးကု</summary>
-  {docData.Cardiologist.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>နှလုံးအထူးကု</summary>
+        {docData.Cardiologist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>တီဘီနှင့်အဆုတ်ရောဂါအထူးကု</summary>
-  {docData.Pulmonologist.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>တီဘီနှင့်အဆုတ်ရောဂါအထူးကု</summary>
+        {docData.Pulmonologist.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-<details>
-  <summary>အရေပြားအထူးကု</summary>
-  {docData.Dermatologists.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>အရေပြားအထူးကု</summary>
+        {docData.Dermatologists.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>ကလေးအထူးကု</summary>
-  {docData.Pediatricians.map((data, key)=>{
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>ကလေးအထူးကု</summary>
+        {docData.Pediatricians.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-  <details>
-  <summary>ခွဲစိတ်အထူးကု</summary>
-  {docData.SurgicalDoctor.map((data, key)=>{
-    
-    return(
-      <div key={key}>
-      <DocInfo 
-      key={key}
-      name ={data.name}
-      specialization = {data.specialization}
-      status = {data.status}
-      facebook = {data.facebook}
-      viber ={data.viber}/>
-      </div>
-    );
-  })}
-  </details>
+      <details>
+        <summary>ခွဲစိတ်အထူးကု</summary>
+        {docData.SurgicalDoctor.map((data, key) => {
+          return (
+            <div key={key}>
+              <DocInfo
+                key={key}
+                name={data.name}
+                specialization={data.specialization}
+                status={data.status}
+                facebook={data.facebook}
+                viber={data.viber}
+              />
+            </div>
+          );
+        })}
+      </details>
 
-<br/>
+      <br />
     </div>
   );
 }
