a7d3646df628d1359d095077b07c14769b24567b
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2021-02-04 19:06:58 +0630

    Updates

---
+++ precache-manifest.5b2f64e8166ef2f6cb3d4f2c1d5de865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0955e31b1f61bd8b82f063c977fb0e0a",
+    "url": "/Democracy_Clinic_Information/index.html"
+  },
+  {
+    "revision": "3cd58902b7787ee42b0b",
+    "url": "/Democracy_Clinic_Information/static/css/main.54947b7b.chunk.css"
+  },
+  {
+    "revision": "d6ddf98f5815355970a4",
+    "url": "/Democracy_Clinic_Information/static/js/2.741578ba.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Democracy_Clinic_Information/static/js/2.741578ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd58902b7787ee42b0b",
+    "url": "/Democracy_Clinic_Information/static/js/main.ece6bfc7.chunk.js"
+  },
+  {
+    "revision": "d7ee7cecd893b4b80d84",
+    "url": "/Democracy_Clinic_Information/static/js/runtime-main.0f4a5d83.js"
+  },
+  {
+    "revision": "1f3fb58d9af60ed9f8517f33b50f34e9",
+    "url": "/Democracy_Clinic_Information/static/media/logo.1f3fb58d.png"
+  }
+]);
